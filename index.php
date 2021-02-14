fb4a28e3937116f3345821be6e074383c6f421a1
Cassio Reis <cassio.reis@engenharia.ufjf.br>
2021-02-14 12:40:10 -0300

    Novas alteracoes

--- index.php
+++ index.php
@@ -795,12 +795,12 @@
  	<div class="footer-bottom">
  		<div class="container">
  			<div class="row justify-content-center align-items-center">
- 				<div class="col-lg-6 col-md-6 col-sm-12">
- 					<p class="copyright-line">© 2020 Dustrian. All rights reserved.</p>
+ 				<div class="col-lg-12 col-md-12 col-sm-12">
+ 					<p class="copyright-line">© 2020 Dustrian. All rights reserved | Oximil Gases LTDA.</p>
  				</div>
- 				<div class="col-lg-6 col-md-6 col-sm-12">
+ 			<!--	<div class="col-lg-6 col-md-6 col-sm-12">
  					<p class="privacy">Privacy Policy | Terms &amp; Conditions</p>
- 				</div>
+ 				</div> -->
  			</div>
  		</div>
  	</div>
